4b34d88010f2242e3fb26dcbf9008fa92a79eb38
Emmanuel Awuah <emmanuelawuah.dev@gmail.com>
2023-08-24 05:49:24 +0000

    initial commit

---
+++ src/test/java/com/emma_ea/secure_todo/SecureTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emma_ea.secure_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
