fe27500f996da0117bbab9537228cce3f3b5438f
Emmanuel Awuah <emmanuelawuah.dev@gmail.com>
2023-08-24 05:50:17 +0000

    fix ignore matchers

---
+++ src/main/java/com/emma_ea/secure_todo/config/TodoRequestStatus.java
@@ -0,0 +1,7 @@
+package com.emma_ea.secure_todo.config;
+
+public class TodoRequestStatus {
+    public static int SUCCESS = 100;
+    public static int SIGNUP_ERROR = 102;
+    public static int SIGIN_ERROR = 101;
+}
---
+++ src/main/java/com/emma_ea/secure_todo/config/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.emma_ea.secure_todo.config;
+
+import com.emma_ea.secure_todo.service.UserAuthService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+import static org.springframework.security.web.util.matcher.RegexRequestMatcher.regexMatcher;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class WebSecurityConfig  {
+
+    private UserAuthService authService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .headers().frameOptions().disable().and()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/v1/auth/**").permitAll()
+                .requestMatchers(antMatcher("/h2/**")).permitAll()
+                .anyRequest().authenticated().and()
+                .authenticationProvider(authenticationProvider());
+
+        return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(authService);
+        return provider;
+    }
+
+}
