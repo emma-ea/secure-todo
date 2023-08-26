559df0ebf85887ab49cfcd91fae3955cc49af369
Emmanuel Awuah <emmanuelawuah.dev@gmail.com>
2023-08-26 16:20:08 +0000

    more meaningful messages :)

--- src/main/java/com/emma_ea/secure_todo/constants/TodoReqStrings.java
+++ src/main/java/com/emma_ea/secure_todo/constants/TodoReqStrings.java
@@ -3,10 +3,11 @@ package com.emma_ea.secure_todo.constants;
 public class TodoReqStrings {
     public static final String USERNAME_NOT_FOUND = "User with email provided does not exist." ;
     public static final String SIGN_IN_FAILED = "Invalid email or password";
+    public static final String SIGN_IN_FAILED_VERIFY_EMAIL = "Please to continue verify your email";
     public static final String SIGN_IN_SUCCESS = "Authentication successful";
     public static final String EMAIL_ALREADY_EXISTS = "Email already exists";
 
     public static String EMAIL_NEED_VERIFICATION(String username) {
-        return String.format("Registration successful %s Please verify email", username);
+        return String.format("Registration successful %s, Please verify email in order to login", username);
     }
 }
